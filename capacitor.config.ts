155d3aa0dd0ab2e50ab1412ed33d6f0b3b660871
Alan Ruiz <matiasruizc315@gmail.com>
2024-07-16 20:14:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pruebas para el examen',
+  webDir: 'www'
+};
+
+export default config;
