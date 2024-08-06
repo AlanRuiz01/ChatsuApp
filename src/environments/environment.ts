92eb41c9121eab215c9d40a9ec63a2d870a00a5e
Alan Ruiz <matiasruizc315@gmail.com>
2024-08-06 07:41:03 -0400

    Ignorar la carpeta src/environments

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
- supabaseUrl: 'https://gtgumyuovlghwjslcppv.supabase.co',
- supabaseKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0Z3VteXVvdmxnaHdqc2xjcHB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0ODk3MzIsImV4cCI6MjAzNzA2NTczMn0.krlp03YPyBlWush2F9dclfTwTEpJ4QpMTamPNAtIGOg'
-};
